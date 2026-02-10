e5a51a7711be693dddfddca74d1fd7f88a3e623e
mouzonghao <mouzonghao@MOUZONGHAO>
2026-02-10 15:10:07 +0800

    查明为什么ppm有问题

--- RayTracing/main.cpp
+++ RayTracing/main.cpp
@@ -12,36 +12,36 @@ color ray_color(const ray& r) {
 
 int main() {
 
-    // Image
+    // 图像纵横比
 
     auto aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
 
-    // Calculate the image height, and ensure that it's at least 1.
+    // 计算图像宽度
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
 
-    // Camera
+    // 相机设置
 
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
     auto viewport_width = viewport_height * (double(image_width) / image_height);
     auto camera_center = point3(0, 0, 0);
 
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    // 计算视口横向和综合矢量
     auto viewport_u = vec3(viewport_width, 0, 0);
     auto viewport_v = vec3(0, -viewport_height, 0);
 
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    // 计算视口横向和综合delta宽度
     auto pixel_delta_u = viewport_u / image_width;
     auto pixel_delta_v = viewport_v / image_height;
 
-    // Calculate the location of the upper left pixel.
+    // 计算第一个视口网格的位置
     auto viewport_upper_left = camera_center
         - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
-    // Render
+    // 渲染器
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
