e113e1b44b93644f181a10a539f6517e5ac81325
mouzonghao <mouzonghao@MOUZONGHAO>
2026-02-12 16:30:15 +0800

    metal

--- RayTracing/camera.h
+++ RayTracing/camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include"hittable.h"
+#include "material.h"
 #include"rtweekend.h"
 #include "color.h"
 #include <fstream>
@@ -88,8 +89,11 @@ private:
         hit_record rec;
         if (world.hit(r, interval(0.0001, infinity), rec))
         {
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.5 * ray_color(ray(rec.p, direction),depth-1,world);
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth - 1, world);
+            return color(0, 0, 0);
         }
         vec3 unit_direction = unit_vector(r.direction());
         auto a = 0.5 * (unit_direction.y() + 1.0);
