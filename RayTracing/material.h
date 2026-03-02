2fdbc08fe03b0b88a0e84401bbf13d770f4f6fad
mouzonghao <mouzonghao@MOUZONGHAO>
2026-03-02 20:51:28 +0800

    动态模糊

--- RayTracing/material.h
+++ RayTracing/material.h
@@ -23,7 +23,7 @@ public:
 
 		if (scatter_direction.near_zero())
 			scatter_direction = rec.normal;
-		scattered = ray(rec.p, scatter_direction);
+		scattered = ray(rec.p, scatter_direction, r_in.time());
 		attenuation = albedo;
 		return true;
 	}
@@ -39,7 +39,7 @@ public:
 	{
 		vec3 reflected = reflect(r_in.direction(), rec.normal);
 		reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
-		scattered = ray(rec.p, reflected);
+		scattered = ray(rec.p, reflected, r_in.time());
 		attenuation = albedo;
 		return (dot(scattered.direction(), rec.normal) > 0);
 	}
@@ -70,7 +70,7 @@ public:
 			direction = reflect(unit_direction, rec.normal);
 		else
 			direction = refract(unit_direction, rec.normal, ri);
-		scattered = ray(rec.p, direction);
+		scattered = ray(rec.p, direction, r_in.time());
 		return true;
 	}
 
